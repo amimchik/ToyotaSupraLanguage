1dd24a41cc5bbc23960b4d016a70143ba0cebe8c
Mark <worldofthegamesprivate@gmail.com>
2025-06-09 23:02:30 +0300

    work on concept

--- include/ToyotaSupraLanguage/Executor.hpp
+++ include/ToyotaSupraLanguage/Executor.hpp
@@ -1,10 +1,28 @@
 #pragma once
 
+#include <ToyotaSupraLanguage/Lexer.hpp>
+#include <ToyotaSupraLanguage/Parser.hpp>
 #include <deque>
+#include <memory>
 #include <string>
 #include <unordered_map>
 
 namespace org::amimchik::tsl::core {
+
+class Reader {
+public:
+    virtual std::string readLine();
+    virtual ~Reader() = default;
+};
+
+class StringReader : public Reader {
+private:
+    std::string str;
+
+public:
+    std::string readLine() override;
+    StringReader(std::string str);
+};
 
 enum class ExecutionContextType {
     IfElse,
@@ -14,39 +32,56 @@ enum class ExecutionContextType {
     Function,
 };
 
-class TSLValue {};
+class Value {};
 
-class ExecutionContext {
+class ExecutionContextNode {
 private:
     ExecutionContextType type;
-    std::unordered_map<std::string, TSLValue> variables;
+    std::unordered_map<std::string, Value> variables;
     std::string name;
+    std::shared_ptr<ExecutionContextNode> parent;
+    std::deque<std::shared_ptr<ExecutionContextNode>> children;
 
 public:
     ExecutionContextType getType();
     bool variableExist(const std::string &name);
-    TSLValue getVariable(const std::string &name);
-    void setVariable(const std::string &name, TSLValue val);
+    Value getVariable(const std::string &name);
+    void setVariable(const std::string &name, Value val);
+    std::shared_ptr<ExecutionContextNode> operator[](size_t i);
 };
 
 class ExecutionContextStack {
 private:
-    std::deque<ExecutionContext> ecs;
+    std::shared_ptr<ExecutionContextNode> top;
 
 public:
-    void push(ExecutionContext executionContext);
-    ExecutionContext pop();
-    ExecutionContext &operator[](size_t i);
-    size_t size();
+    void push(std::shared_ptr<ExecutionContextNode> n);
+    std::shared_ptr<ExecutionContextNode> pop();
+    std::shared_ptr<ExecutionContextNode> operator[](size_t i);
+};
+
+class Thread {
+private:
+    ExecutionContextStack ecs;
+    std::deque<std::shared_ptr<Thread>> dependencies;
+    std::shared_ptr<Thread> waitingNow;
+
+public:
+    bool completed();
+    Reader reader;
 };
 
 class Executor {
 private:
-    ExecutionContextStack ecs;
+    ExecutionContextNode ecsBottom;
+    Parser parser;
+    Lexer lexer;
+    std::deque<Thread> threads;
+    size_t currentThreadIndex;
 
 public:
     Executor();
-    void executeLine(std::string line);
+    void eventLoopIteration();
 };
 
 }  // namespace org::amimchik::tsl::core
